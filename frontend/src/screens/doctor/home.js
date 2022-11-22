61e81c0487dec843fd6257f85b075c92f101dd4b
Sreehari vr <sreeharivr855@gmail.com>
2022-11-22 22:05:56 +0530

    feat: minor changes

--- frontend/src/screens/doctor/home.js
+++ frontend/src/screens/doctor/home.js
@@ -1,15 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
 import './doctor.css';
 import { Navbar } from './Navbar';
 import { ListBox } from './listBox';
 import { Heading } from './heading';
 
 export const DoctorHome=()=>{
-    return(
-        <div>
-           <Navbar />
-           <Heading />
-           <ListBox />
-           
-        </div>
-    )
+const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  return(
+    <div>
+        {loading?(
+            <div className="loader-container">
+                <div className="spinner"></div>
+
+            </div>
+
+        ):(
+
+            <>
+                <Navbar />
+                <Heading />
+                <ListBox />
+            </>
+
+        )}
+    </div>
+  )
+  
+    
 }
